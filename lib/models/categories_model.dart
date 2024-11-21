768a888092623650fe621b02852c666668735cab
Rio <rioloka86@gmail.com>
2024-11-21 19:41:40 +0700

    feat: add categories model and icons

---
+++ lib/models/categories_model.dart
@@ -0,0 +1,13 @@
+const categoriesData = {
+  'Foods & Drinks': 'assets/images/categories/foods_and_drinks.svg',
+  'Shopping': 'assets/images/categories/shopping.svg',
+  'Housing': 'assets/images/categories/housing.svg',
+  'Transportation': 'assets/images/categories/transportation.svg',
+  'Vehicle': 'assets/images/categories/vehicle.svg',
+  'Life & Entertainment': 'assets/images/categories/life_and_entertainment.svg',
+  'Communication & PC': 'assets/images/categories/comms_and_pc.svg',
+  'Financial Expenses': 'assets/images/categories/financial_expenses.svg',
+  'Investments': 'assets/images/categories/investments.svg',
+  'Income': 'assets/images/categories/income.svg',
+  'Others': 'assets/images/categories/others.svg',
+};
