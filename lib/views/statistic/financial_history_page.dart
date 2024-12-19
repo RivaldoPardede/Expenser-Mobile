5c087574c3a7d0b6313fa85f10afe725e4b2e291
FrederickGodiva <godivafrederick@gmail.com>
2024-12-19 14:21:30 +0700

    feat(statistic): expense graph

--- lib/views/statistic/financial_history_page.dart
+++ lib/views/statistic/financial_history_page.dart
@@ -1,5 +1,7 @@
-import 'package:final_project/views/common/expense_graph.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:final_project/services/firestore_service.dart';
+import 'package:final_project/views/common/expense_graph.dart';
 import 'widgets/financial_health_pie.dart';
 import 'widgets/financial_health_overview.dart';
 
@@ -8,6 +10,8 @@ class FinancialHistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final FirestoreService firestoreService = FirestoreService();
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: CustomScrollView(
@@ -30,98 +34,142 @@ class FinancialHistoryPage extends StatelessWidget {
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const ExpenseGraph(type: ExpenseType.weekly),
-                  const SizedBox(height: 16),
-                  Container(
-                    padding: const EdgeInsets.all(35),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 10,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // Pie Chart
-                            Expanded(
-                              flex: 2,
-                              child: FinancialHealthPie(),
-                            ),
-                            const SizedBox(width: 30),
-                            Expanded(
-                              flex: 1,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Container(
-                                        width: 14,
-                                        height: 14,
-                                        decoration: const BoxDecoration(
-                                          color: Color(0xFF7BB2E8),
-                                          shape: BoxShape.circle,
-                                        ),
-                                      ),
-                                      const SizedBox(width: 6),
-                                      const Text(
-                                        "Savings",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        width: 14,
-                                        height: 14,
-                                        decoration: const BoxDecoration(
-                                          color: Color(0xFF114A83),
-                                          shape: BoxShape.circle,
-                                        ),
-                                      ),
-                                      const SizedBox(width: 6),
-                                      const Text(
-                                        "Expenses",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+              child: StreamBuilder<List<Map<String, dynamic>>>(
+                stream: firestoreService.getAccountsStream(
+                    FirebaseAuth.instance.currentUser!.uid),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  if (snapshot.hasError || !snapshot.hasData) {
+                    return const Center(
+                      child: Text('Error fetching data'),
+                    );
+                  }
+
+                  final accounts = snapshot.data!;
+                  double totalIncome = 0.0;
+                  double totalSavings = 0.0;
+                  double totalExpenses = 0.0;
+
+                  for (var account in accounts) {
+                    final balance = (account['current_balance'] ?? 0.0) as double;
+                    final type = account['type'] ?? '';
+                    if (type == 'Savings') {
+                      totalSavings += balance;
+                    } else if (type == 'Expenses') {
+                      totalExpenses += balance;
+                    } else if (type == 'Income') {
+                      totalIncome += balance;
+                    }
+                  }
+
+                  final savingsPercentage =
+                  totalIncome > 0 ? (totalSavings / totalIncome) * 100 : 0.0;
+                  final expendituresPercentage =
+                  totalIncome > 0 ? (totalExpenses / totalIncome) * 100 : 0.0;
+
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const ExpenseGraph(type: ExpenseType.weekly),
+                      const SizedBox(height: 16),
+                      Container(
+                        padding: const EdgeInsets.all(35),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              offset: const Offset(0, 2),
                             ),
                           ],
                         ),
-                        const SizedBox(height: 30),
-                        const FinancialHealthOverview(
-                          financialHealthScore: 52.6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // Pie Chart
+                                Expanded(
+                                  flex: 2,
+                                  child: FinancialHealthPie(
+                                    savingsPercentage: savingsPercentage,
+                                    expendituresPercentage: expendituresPercentage,
+                                  ),
+                                ),
+                                const SizedBox(width: 30),
+                                Expanded(
+                                  flex: 1,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 14,
+                                            height: 14,
+                                            decoration: const BoxDecoration(
+                                              color: Color(0xFF7BB2E8),
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 6),
+                                          const Text(
+                                            "Savings",
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 14,
+                                            height: 14,
+                                            decoration: const BoxDecoration(
+                                              color: Color(0xFF114A83),
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 6),
+                                          const Text(
+                                            "Expenses",
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 30),
+                            FinancialHealthOverview(
+                              financialHealthScore:
+                              (savingsPercentage + expendituresPercentage) / 2,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
           ),
--- lib/views/statistic/widgets/financial_health_pie.dart
+++ lib/views/statistic/widgets/financial_health_pie.dart
@@ -2,32 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class FinancialHealthPie extends StatelessWidget {
+  final double savingsPercentage;
+  final double expendituresPercentage;
+
+  const FinancialHealthPie({
+    super.key,
+    required this.savingsPercentage,
+    required this.expendituresPercentage,
+  });
+
   @override
   Widget build(BuildContext context) {
+    final totalPercentage = savingsPercentage + expendituresPercentage;
+
     return Container(
       height: 180,
       width: 180,
       child: PieChart(
         dataMap: {
-          "Savings": 67,
-          "Expenditures": 33,
+          "Savings": savingsPercentage,
+          "Expenditures": expendituresPercentage,
         },
         animationDuration: const Duration(milliseconds: 800),
         chartType: ChartType.ring,
         ringStrokeWidth: 50,
-        baseChartColor: Colors.grey,
+        baseChartColor: Colors.grey[200]!,
         colorList: const [
-          Color(0xFF7BB2E8),
-          Color(0xFF114A83),
+          Color(0xFF7BB2E8), // Savings
+          Color(0xFF114A83), // Expenditures
         ],
         chartValuesOptions: ChartValuesOptions(
           showChartValueBackground: true,
-          chartValueBackgroundColor: const Color(0xFFFFFFFF)
-              .withOpacity(0.67),
+          chartValueBackgroundColor: const Color(0xFFFFFFFF).withOpacity(0.67),
           showChartValuesInPercentage: true,
           showChartValuesOutside: false,
           chartValueStyle: const TextStyle(
-            fontSize: 24,
+            fontSize: 14,
             fontWeight: FontWeight.bold,
             color: Color(0xFF012169),
           ),
@@ -35,6 +45,7 @@ class FinancialHealthPie extends StatelessWidget {
         legendOptions: const LegendOptions(
           showLegends: false,
         ),
+        totalValue: totalPercentage,
       ),
     );
   }
